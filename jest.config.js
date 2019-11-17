754049ffce4766e78c489ed8ba1ee4f8e495be96
Alex Rossi Ignez <rx.rossi@gmail.com>
2019-11-17 13:42:41 +0000

    Add test coverage config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,13 @@ module.exports = {
   preset: "ts-jest",
   clearMocks: true,
   testEnvironment: "node",
-  testPathIgnorePatterns: ["/node_modules/", "/build/"]
+  testPathIgnorePatterns: ["/node_modules/", "/build/"],
+  coverageThreshold: {
+    global: {
+      statements: 90,
+      branches: 50,
+      functions: 90,
+      lines: 90
+    }
+  }
 }
-
